f2251929c9ab34e81a51822edc95762480dc4419
Tom Hartman <thomas.lees.hartman@gmail.com>
2016-11-14 11:47:11 -0500

    Circuit init
    Added an initialization/load file for the circuit module.

---
+++ init.lua
@@ -0,0 +1,26 @@
+-------------------------------------------------------------------------------
+-- init.lua for circuit                                                      --
+-- Copyright (c) 2016 Tom Hartman (thomas.lees.hartman@gmail.com)            --
+--                                                                           --
+-- This program is free software; you can redistribute it and/or             --
+-- modify it under the terms of the GNU General Public License               --
+-- as published by the Free Software Foundation; either version 2            --
+-- of the License, or the License, or (at your option) any later             --
+-- version.                                                                  --
+--                                                                           --
+-- This program is distributed in the hope that it will be useful,           --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of            --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             --
+-- GNU General Public License for more details.                              --
+-------------------------------------------------------------------------------
+
+--- Commentary -- {{{
+-- Setup and return a ciruit object that can be used to create and interacte
+-- with the circuit awesome module
+-- }}}
+
+-- init circuit -- {{{
+local circuit = { grid = require('grid'), }
+
+return circuit
+--
